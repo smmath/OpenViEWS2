ce1422593e64c423f4c4c2cc71772c1fdd5bb32f
Mathew Smith <mtjs@protonmail.com>
2020-09-22 12:14:05 +0200

    Update install_views2.sh to use google drive directory

--- install_views2.sh
+++ install_views2.sh
@@ -12,20 +12,20 @@ echo "Removing existing views2 env"
 conda remove --name views2 --all --yes
 echo "Creating env from env_static.yaml"
 # @TODO: Change back to env_static.yaml asap when we have working "builds" for linux
-conda env create -f misc/environment.yaml
+conda env create -f /content/drive/My\ Drive/OpenViEWS2/misc/environment.yaml
 echo "Activating env"
-conda activate views2
+source activate views2
 echo "Running pip install --editable . to install the views package"
-pip install --editable .
+pip install --editable /content/drive/My\ Drive/OpenViEWS2
 
 echo "Creating storage directory here"
 mkdir -p ./storage
 
 # Copy the default config file to default config dir ~/.views2/
-if [ ! -f ./config.yaml ];
+if [ ! -f /content/drive/My\ Drive/OpenViEWS2/config.yaml ];
     then
         echo "No current ./config.yaml found, copying the defaults"
-        cp ./misc/defaults.yaml ./config.yaml
+        cp /content/drive/My\ Drive/OpenViEWS2/misc/defaults.yaml /content/drive/My\ Drive/OpenViEWS2/config.yaml
     else
         echo "./config.yaml already exists, not changing it"
 fi
